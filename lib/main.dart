0e3a01677d3bb1708efe17158f0fff7925a887d4
Jonas Schneider <jonas.max.schneider@gmail.com>
2022-02-04 21:15:16 +0100

    CupertinoTut

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,23 @@ import 'package:flutter/material.dart';
 import "package:flutter/cupertino.dart";
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'package:time/product_list_tab.dart';
+
+import 'model/app_state_model.dart';
 
 void main() {
   LicenseRegistry.addLicense(() async* {
-    final LatoLicense = await rootBundle.loadString("google_fonts/Lato-OFL.txt");
-    final PlayfairLicense = await rootBundle.loadString("google_fonts/Playfair-OFL.txt");
+    final LatoLicense =
+        await rootBundle.loadString("google_fonts/Lato-OFL.txt");
+    final PlayfairLicense =
+        await rootBundle.loadString("google_fonts/Playfair-OFL.txt");
     yield LicenseEntryWithLineBreaks(['google_fonts'], LatoLicense);
   });
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider<AppStateModel>(
+    create: (_) => AppStateModel(),
+    child: const MyApp(),  
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -34,11 +43,38 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-        child: Center(
-            child: Text(
-      "Hello World",
-      style: GoogleFonts.playfairDisplay(fontSize: 50, fontWeight: FontWeight.bold),
-    )));
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(items: [
+        BottomNavigationBarItem(icon: Icon(CupertinoIcons.home)),
+        BottomNavigationBarItem(icon: Icon(CupertinoIcons.clock)),
+      ],),
+      tabBuilder: (context, index) {
+        late final CupertinoTabView returnValue;
+        switch (index) {
+          case 0:
+            returnValue = CupertinoTabView(builder: (context) {
+              return const CupertinoPageScaffold(
+                child: ProductListTab(),
+              );
+            });
+            break;
+          case 1:
+            returnValue = CupertinoTabView(builder: (context) {
+              return const CupertinoPageScaffold(
+                child: SearchTab(),
+              );
+            });
+            break;
+          case 2:
+            returnValue = CupertinoTabView(builder: (context) {
+              return const CupertinoPageScaffold(
+                child: ShoppingCartTab(),
+              );
+            });
+            break;
+        }
+        return returnValue;
+      },  
+    )    
   }
 }
